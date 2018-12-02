0c86027b3ca72d4bf82202a260c227e75ddc160b
Jason Evarts <jason.evarts@colorado.edu>
2018-12-01 21:21:46 -0800

    fixed routing for good login

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,13 +4,13 @@ var passport = require('passport');
 
 app.get('/', function (request, response) {
    // render the views/index.ejs template file
-   response.render('index', {title: 'Lab 9 - Integration using Node.js'})
+   response.render('index', {title: 'Boulder Meal Slackerz'})
 });
 
 app.post('/login', 
   passport.authenticate('local', { failureRedirect: '/' }),
   function(req, res) {
-    res.redirect('/store');
+    res.redirect('/home');
   });
 
 module.exports = app;
