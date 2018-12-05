cf46682ce58b3505ceefadc865d5a90328e1f4e2
Stian Howard <stianhoward@gmail.com>
2018-12-04 17:27:19 -0700

    Test for our database format

--- server/conf/auth.js
+++ server/conf/auth.js
@@ -9,13 +9,13 @@ passport.use(new Strategy(
       db.users.findByUsername(username, function(err, user) {
         if (err) { return cb(err); }
         if (!user) { return cb(null, false); }
-        if (user.password != password) { return cb(null, false); }
+        if (user.Password != password) { return cb(null, false); }
         return cb(null, user);
       });
     }));
 
 passport.serializeUser(function(user, cb) {
-    cb(null, user.id);
+    cb(null, user.UserID);
 });
     
 passport.deserializeUser(function(id, cb) {
--- server/db/users.js
+++ server/db/users.js
@@ -1,20 +1,14 @@
-var records = [
+/* var records = [
     { id: 1, username: 'jack', password: 'secret', displayName: 'Jack', emails: [ { value: 'jack@example.com' } ] }
   , { id: 2, username: 'jill', password: 'birthday', displayName: 'Jill', emails: [ { value: 'jill@example.com' } ] }
+]; */
+
+var records = [
+    {UserID: 1, UserEmail: 'test@gmail.com', username:'Stian',Password:'test', Displayname:'Stian'}
+
 ];
 
 var db = require('../database');
-var query = 'SELECT * FROM store;';
-db.any(query)
-  .then(function (rows) {
-    //console.log(rows);
-  })
-  .catch(function (err) {
-    console.log(err);
-    //Figure out how the hell to handle this error of not return good data
-    //Probably return it as a null or something like that, then let passport deal with it
-    //along with a console.log of the error
-  })
 
 exports.findById = function(id, cb) {
   process.nextTick(function() {
@@ -27,7 +21,28 @@ exports.findById = function(id, cb) {
   });
 }
 
-exports.findByUsername = function(username, cb) {
+exports.findByDisplayname = function(displayname, cb) {
+/*   var query = 'SELECT * FROM UserTable where $1 = Displayname;';
+  db.any(query, displayname)
+  .then(function (rows) {
+    var records = rows;
+    process.nextTick(function() {
+      for (var i = 0, len = records.length; i < len; i++) {
+        var record = records[i];
+        if (record.username === username) {
+          return cb(null, record);
+        }
+      }
+      return cb(null, null);
+    });
+  })
+  .catch(function (err) {
+    console.log(err);
+    //Figure out how the hell to handle this error of not return good data
+    //Probably return it as a null or something like that, then let passport deal with it
+    //along with a console.log of the error
+  }) */
+
   process.nextTick(function() {
     for (var i = 0, len = records.length; i < len; i++) {
       var record = records[i];
@@ -38,3 +53,40 @@ exports.findByUsername = function(username, cb) {
     return cb(null, null);
   });
 }
+
+exports.findByEmail = function(email, cb) {
+  var query = 'SELECT * FROM UserTable where ;';
+  db.one(query)
+  .then(function (rows) {
+    var records = rows;
+    process.nextTick(function() {
+      for (var i = 0, len = records.length; i < len; i++) {
+        var record = records[i];
+        if (record.UserEmail === email) {
+          return cb(null, record);
+        }
+      }
+      return cb(null, null);
+    });
+  })
+  .catch(function (err) {
+    console.log(err);
+    return cb(null, null);
+    //Figure out how the hell to handle this error of not return good data
+    //Probably return it as a null or something like that, then let passport deal with it
+    //along with a console.log of the error
+  })
+}
+
+exports.findByUsername = function(username, cb) {
+
+  process.nextTick(function() {
+    for (var i = 0, len = records.length; i < len; i++) {
+      var record = records[i];
+      if (record.Displayname === username) {
+        return cb(null, record);
+      }
+    }
+    return cb(null, null);
+  });
+};
