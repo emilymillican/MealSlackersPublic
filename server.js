a1aed279c1af0f6d60e16072a6cc01e909a803fb
Jason Evarts <jason.evarts@colorado.edu>
2018-12-09 23:56:25 -0800

    changed port for http back to 4000

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ var home = require('./routes/home');
 app.use('/', login);
 app.use('/home', home);
 
-var port = process.env.PORT;
+var port = '4000';
 app.listen(port, function () {
     console.log('Server running on http://localhost:' + port)
 });
