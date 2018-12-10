9866669f57826537f2ade077c293dba7b4f35359
Jason Evarts <jason.evarts@colorado.edu>
2018-12-10 00:12:35 -0800

    added test for logged in user access to home

--- tests/logging.js
+++ tests/logging.js
@@ -21,3 +21,17 @@ before(function(done){
       done();
     });
 });
+
+describe('GET /home', function(done){
+//addresses 1st bullet point: if the user is logged in we should get a 200 status code
+  it('should return a 200 response if the user is logged in', function(done){
+    authenticatedUser.get('/home')
+    .expect(200, done);
+  });
+//addresses 2nd bullet point: if the user is not logged in we should get a 302 response code and be directed to the /login page
+  it('should return a 302 response and redirect to /login', function(done){
+    request(app).get('/home')
+    .expect('Location', '/')
+    .expect(302, done);
+  });
+});
