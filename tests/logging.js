4bcc1b66fd3bf9e65fe620d6686f2d2729728537
Jason Evarts <jason.evarts@colorado.edu>
2018-12-10 16:38:23 -0800

    changed from json to string for user-pass

--- tests/logging.js
+++ tests/logging.js
@@ -16,7 +16,7 @@ before(function(done){
     .post('/login')
     .set('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8')
     .set('Content-Type','application/x-www-form-urlencoded')
-    .send(userCredentials)
+    .send('username=Stian@colorado.edu&password=irock')
     .expect('Content-Type', /json/)
     .expect(200)
     .end(function(err, response){
